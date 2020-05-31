3f7abf8a2f7c7c71dcbf530ea408248d1fbb8420
Gimpei Misono <ginpeimisonou@gmail.com>
2020-05-31 11:55:08 +0900

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
+  # GET /login
   def new
+  end
+
+  # POST login
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  # DELETE /logout
+  def destroy
+    log_out
+    # 慣習的に root_path ではなく、root_url と記載している
+    redirect_to root_url
   end
 end
