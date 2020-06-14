684a79d3c574970eb389fd35fcfa1849ef79155d
Gimpei Misono <ginpeimisonou@gmail.com>
2020-06-14 11:20:52 +0900

    Implement delte micropost action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,15 +61,6 @@ class UsersController < ApplicationController
   end
 
   # before action
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
-
   # 正しいユーザーかどうか確認
   def correct_user
     @user = User.find(params[:id])
